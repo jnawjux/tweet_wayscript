f3a696cb0dc22e9c1778a79161edfdd1d4623175
John Naujoks <johnnaujoks@gmail.com>
2019-11-21 14:43:24 -0800

    Add smptlib to send message with tweet, add send info

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 # Import packages and config
 from tweepy import OAuthHandler, API
-from config import consumer_key, consumer_secret, access_token, access_token_secret
+from config import consumer_key, consumer_secret, access_token, access_token_secret, app_pw, from_mail, to_mail
 import re
 
 auth = OAuthHandler(consumer_key, consumer_secret)
@@ -13,5 +13,22 @@ r = re.compile("(.*morning|.*night)")
 miranda = api.user_timeline('@Lin_Manuel')
 tweets = [status.text for status in miranda]
 newlist = list(filter(r.match, tweets))
-for t in newlist:
-    print(t)
+message =  ", ".join(newlist)
+
+import smtplib 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login(from_mail, app_pw) 
+  
+  
+# sending the mail 
+s.sendmail(from_mail, to_mail, message.encode("utf8")) 
+  
+# terminating the session 
+s.quit() 
