24b074fe1973c72dcf204b9aac1a4db56f1a9c90
John Naujoks <johnnaujoks@gmail.com>
2019-11-21 16:38:34 -0800

    Update to log tweet id to avoid replication

--- search.py
+++ search.py
@@ -1,34 +1,36 @@
 # Import packages and config
 from tweepy import OAuthHandler, API
 from config import consumer_key, consumer_secret, access_token, access_token_secret, app_pw, from_mail, to_mail
+import smtplib
 import re
+from email.mime.text import MIMEText
 
+# Authenticate Tweepy connection
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = API(auth)
 
-r = re.compile("(.*morning|.*night)")
- 
-miranda = api.user_timeline('@Lin_Manuel')
-tweets = [status.text for status in miranda]
-newlist = list(filter(r.match, tweets))
-message =  ", ".join(newlist)
-
-import smtplib 
-  
-# creates SMTP session 
+# Login & Authentification of email
 s = smtplib.SMTP('smtp.gmail.com', 587) 
-  
-# start TLS for security 
 s.starttls() 
-  
-# Authentication 
-s.login(from_mail, app_pw) 
-  
-  
-# sending the mail 
-s.sendmail(from_mail, to_mail, message.encode("utf8")) 
-  
-# terminating the session 
+s.login(from_mail, app_pw)
+
+# Open log
+f = open('log.txt', 'r+')
+logged_tweets = [line for line in f.readlines()]
+
+# Getting tweets
+miranda_tweets = api.user_timeline('@Lin_Manuel')
+
+# If tweet matches, checking log, adding if new, and sending.
+for status in miranda_tweets:
+    if re.search("(.*morning|.*night)", status.text) != None:
+        if status.id_str not in logged_tweets:
+            f.writelines("\n" + status.id_str)
+            msg = MIMEText("Good Morning / Good Night: \nhttps://www.twitter.com/lin_manuel/statuses/" + status.id_str)
+            msg['From'] = from_mail
+            msg['To'] = to_mail
+            s.sendmail(from_mail, to_mail, msg.as_string())
+# Terminating the session 
 s.quit() 
+f.close()
